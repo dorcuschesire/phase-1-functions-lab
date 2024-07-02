98c127c6ba0621f6e3c22ba418d2df76d848c7cd
dorcuschesire <chesiredorcus@gmail.com>
2024-07-02 14:22:04 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pick){
+    let headquaters=42
+    if(pick > headquaters){
+        let actualDistance = pick-headquaters
+        return actualDistance
+    }else if (pick < headquaters){
+        return headquaters - pick
+    }else {
+        return 'in the same block'
+    }
+}
+function distanceFromHqInFeet(pick){
+    return distanceFromHqInBlocks(pick)*264
+}
+function distanceTravelledInFeet(start, destination){
+    const blockTravelled= Math.abs(start-destination);
+    return blockTravelled * 264;
+}
+   function calculatesFarePrice(start, destination){
+    const feetTravelled = distanceTravelledInFeet(start, destination);
+    if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled > 400 && feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled > 2000 && feetTravelled <= 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+   }
